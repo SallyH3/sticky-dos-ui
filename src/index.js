0e781c8a9e6e7263b54efae8d7fb1b23529ee72a
Sally Haefling <sallyhaefling@gmail.com>
2019-05-20 15:24:33 -0600

    Initial commit Co-authored-by: Justin Pyktel <justinpyktel@gmail.com> Co-authored-by: Lauren Boyer <lboyer4@outlook.com>

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers';
+import { BrowserRouter } from 'react-router-dom';
+
+const store = createStore(rootReducer, composeWithDevTools())
+
+
+ReactDOM.render(
+  <Provider store = { store }>
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
+  </Provider>,
+document.getElementById('root'));
+
+serviceWorker.unregister();
