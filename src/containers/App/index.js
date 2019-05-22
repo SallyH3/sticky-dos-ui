c5a26e734145bf72cd60e1a67941458264c35fec
Sally Haefling <sallyhaefling@gmail.com>
2019-05-21 19:18:10 -0600

    Add componentDidMount and postTest function
    Co-authored-by: Justin Pyktel <justinpyktel@gmail.com>
    Co-authored-by: Lauren Boyer <lboyer4@outlook.com>

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sticky Dos</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cardList: []
+    }
+  }
+
+componentDidMount = () => {
+  const url = 'http://localhost:3000/api/v1/cardList'
+  fetch(url)
+  .then(response => response.json())
+  .then(cardList => this.setState({ cardList }))
+  .catch(error => alert('Error fetching data'))
+}
+
+postTest = () => {
+  const sample = {
+    id: 3,
+    title: 'yo',
+    content: [{
+      id: 1,
+      type: 'string',
+      text: 'sample string',
+      checked: null
+    }]
+  }
+}
+
+  render() {
+    console.log('state', this.state.cardList)
+    return (
+      <div className="App">
+        <h1>Sticky Dos</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
