66276c9de1a3d98ac8261e8e91c80d7b539fa116
SiimonStark <justinpyktel@gmail.com>
2019-05-23 15:24:50 -0600

    Handle title & string input
    Co-authored-by: Sally Haefling <sallyhaefling@gmail.com>

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -7,7 +7,7 @@ class Form extends Component {
       expanded: false,
       title: '',
       body: [
-        {id: 0, content: '', type: 'string'}
+        {}
       ]
     };
   }
@@ -16,12 +16,24 @@ class Form extends Component {
     this.setState({ expanded: true });
   };
 
-  handleTitleChange = () => {
+  handleTitleChange = (e) => {
+    let {name, value} = e.target;
 
+    this.setState({[name]: value})
   }
 
-  handleStrChange = () => {
-
+  handleStrChange = (e) => {
+    let {name, value} = e.target;
+    console.log(name)
+    this.setState({
+      body: [
+        {
+          content: value,
+          id: Date.now(),
+          type: name
+        }
+      ]
+    });
   }
 
   handleListChange = () => {
@@ -47,20 +59,31 @@ class Form extends Component {
     // ?? If user checks checkList, render input w/ class of listItem
       // ?? Else render input w/ class of stringItem
 
+// Todo: handleChanges on inputs, set to state
+// - Then, send in fetch POST
+
     let expandedForm = (
       <form onSubmit={this.handleSubmit}>
         <input
+          name="title"
           onChange={this.handleTitleChange}
           type="text"
           placeholder="Enter Title"
           value={this.state.title}
         />
-        <input type="text" placeholder="Enter Body" />
+        <input 
+          onChange={this.handleStrChange} 
+          name="stringItem" 
+          type="text" 
+          value={this.state.body[0].content} 
+          placeholder="Enter Body" />
         <label for="checkList"> Click for List Item </label>
         <input type="checkbox" id="checkList" />
         <input type="submit" />
       </form>
     );
+
+      console.log('FormState', this.state)
 
     return (
       <section>
