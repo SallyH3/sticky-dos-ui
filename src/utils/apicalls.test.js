7c72a916c1b1a64a9ece769528de657333cb0905
Lauren Boyer <lboyer4@outlook.com>
2019-05-24 19:11:54 -0600

    added tests for the post fetch function in api calls

--- src/utils/apicalls.test.js
+++ src/utils/apicalls.test.js
@@ -1,4 +1,4 @@
-import { fetchCardList } from './apicalls';
+import { fetchCardList, postFetch } from './apicalls';
 
 describe('fetchCardList', () => {
 	let mockCardListResponse;
@@ -48,3 +48,70 @@ describe('fetchCardList', () => {
 		await expect(fetchCardList()).rejects.toEqual(Error('Error fetching data'))
 	});
 });
+
+describe('postFetch', () => {
+	let mockCardListResponse;
+	let mockInit;
+	let mockUrl;
+		beforeEach(() => {
+
+		mockUrl = 'http://localhost:3001/api/v1/cardList';
+
+		mockInit = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        title: 'hi there',
+        content: 'bye there'
+      })
+    }
+
+		mockCardListResponse = [
+			{
+				id: 1,
+				title: 'hello world',
+				content: 'have a good day'
+			},
+			{
+				id: 2,
+				title: 'goodbye world',
+				content: 'have a good night'
+			},
+			{
+				id: 3,
+				title: 'hi there',
+        content: 'bye there'
+			}
+
+		];
+
+		window.fetch=jest.fn().mockImplementation(() => {
+			return Promise.resolve({
+				ok: true,
+				json: ()=> Promise.resolve(mockCardListResponse)
+			})
+		});	
+	});
+
+	it('should be called with the correct params', () => {
+		const expectedA = mockUrl;
+		const expectedB = mockInit
+		postFetch(mockUrl, mockInit);
+		expect(window.fetch).toHaveBeenCalledWith(expectedA, expectedB);
+	});
+
+	it('should return a response if the status is ok', async () => {
+		const result = await postFetch();
+		expect(result).toEqual(mockCardListResponse)
+	});
+
+	it('should return an error if status is not ok', async () => {
+		window.fetch=jest.fn().mockImplementation(() => {
+			return Promise.resolve({
+				ok: false
+			})
+		});
+		await expect(postFetch()).rejects.toEqual(Error('Error posting data'))
+	});
+
+})
